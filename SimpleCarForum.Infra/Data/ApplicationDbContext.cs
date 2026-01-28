515b1c5a171341254eec44f79f43fc69090cdac2
AlexAndonov <114021498+AlexAndonov@users.noreply.github.com>
2026-01-28 12:02:00 +0200

    Add DbSets for comment,post and category

--- SimpleCarForum.Infra/Data/ApplicationDbContext.cs
+++ SimpleCarForum.Infra/Data/ApplicationDbContext.cs
@@ -10,5 +10,9 @@ namespace SimpleCarForum.Data
           : base(options)
         {
         }
+
+        public DbSet<Post> Posts { get; set; }
+        public DbSet<Comment> Comments { get; set; }
+        public DbSet<Category> Categories { get; set; }
     }
 }
