bee1d6d1b8eda195fed23c67bdd1d63969f6317d
AlexAndonov <114021498+AlexAndonov@users.noreply.github.com>
2026-01-28 14:47:51 +0200

    Add IPostService methods

---
+++ SimpleCarForum.Core/Contracts/IPostService.cs
@@ -0,0 +1,16 @@
+﻿using SimpleCarForum.Core.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SimpleCarForum.Core.Contracts
+{
+    public interface IPostService
+    {
+        Task<IEnumerable<PostDto>> GetAllAsync();
+        Task<PostDto?> GetById(Guid id);
+        Task<PostDto> Create(PostCreateDto model, string userId);
+        Task<PostDto?> Update(PostEditDto model);
+        Task<bool> Delete(Guid id);
+    }
+}
