e6c38faf038ed22b438ef556b3c1dc9fdfabede3
AlexAndonov <114021498+AlexAndonov@users.noreply.github.com>
2026-01-28 14:26:50 +0200

    Add comment DTO

---
+++ SimpleCarForum.Core/ViewModels/CommentDto.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SimpleCarForum.Infra.Data.Models;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using static SimpleCarForum.Infra.Data.Constants.ValidationConstants;
+
+namespace SimpleCarForum.Core.ViewModels
+{
+    public class CommentDto
+    {
+        public Guid Id { get; set; }
+
+        [Required]
+        [StringLength(CommentContentMaxLength, MinimumLength = CommentContentMinLength, ErrorMessage = StringLengthErrorMessage)]
+        public string Content { get; set; } = null!;
+
+        public string AuthorName{ get; set; } = null!;
+
+        public DateTime CreatedOn { get; set; } = DateTime.UtcNow;
+    }
+}
