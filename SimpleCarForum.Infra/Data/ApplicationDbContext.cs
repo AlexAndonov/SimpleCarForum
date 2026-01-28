c9a79cb6b5a353604e33df9fb049ca61eb29c5d1
AlexAndonov <114021498+AlexAndonov@users.noreply.github.com>
2026-01-28 16:58:22 +0200

    Fix cascade delete configuration between Post, Comment and Category

--- SimpleCarForum.Infra/Data/ApplicationDbContext.cs
+++ SimpleCarForum.Infra/Data/ApplicationDbContext.cs
@@ -11,6 +11,22 @@ namespace SimpleCarForum.Data
         {
         }
 
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+
+            builder.Entity<Comment>()
+                .HasOne(c => c.Post)
+                .WithMany(p => p.Comments)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            builder.Entity<Post>()
+                .HasOne(p => p.Category)
+                .WithMany(c => c.Posts)
+                .OnDelete(DeleteBehavior.NoAction);
+        }
+
         public DbSet<Post> Posts { get; set; }
         public DbSet<Comment> Comments { get; set; }
         public DbSet<Category> Categories { get; set; }
