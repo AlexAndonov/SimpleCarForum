a49f995b241f6f1f5ab66536d7bef287eb1c72d4
AlexAndonov <114021498+AlexAndonov@users.noreply.github.com>
2026-01-28 14:26:07 +0200

    Add post DTOs

---
+++ SimpleCarForum.Core/ViewModels/PostDto.cs
@@ -0,0 +1,33 @@
+﻿using SimpleCarForum.Infra.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+using static SimpleCarForum.Infra.Data.Constants.ValidationConstants;
+
+namespace SimpleCarForum.Core.ViewModels
+{
+    public class PostDto
+    {
+        public Guid Id { get; set; }
+
+        [Required(ErrorMessage = RequiredFieldMessage)]
+        [StringLength(PostTitleMaxLength, MinimumLength = PostTitleMinLength, ErrorMessage = StringLengthErrorMessage)]
+        public string Title { get; set; } = null!;
+
+
+        [Required(ErrorMessage = RequiredFieldMessage)]
+        [StringLength(PostContentMaxLength, MinimumLength = PostContentMinLength, ErrorMessage = StringLengthErrorMessage)]
+        public string Content { get; set; } = null!;
+
+        public DateTime CreatedOn { get; set; }
+
+        public string AuthorId { get; set; } = null!;
+        public string AuthorName { get; set; } = null!;
+
+        public int CategoryId { get; set; }
+        public string CategoryName { get; set; } = null!;
+
+        public List<CommentDto> Comments { get; set; } = new List<CommentDto>();
+    }
+}
---
+++ SimpleCarForum.Core/ViewModels/PostEditDto.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+using static SimpleCarForum.Infra.Data.Constants.ValidationConstants;
+
+namespace SimpleCarForum.Core.ViewModels
+{
+    public class PostEditDto
+    {
+        public Guid Id { get; set; }
+
+        [Required(ErrorMessage = RequiredFieldMessage)]
+        [StringLength(PostTitleMaxLength, MinimumLength = PostTitleMinLength, ErrorMessage = StringLengthErrorMessage)]
+        public string Title { get; set; } = null!;
+
+
+        [Required(ErrorMessage = RequiredFieldMessage)]
+        [StringLength(PostContentMaxLength, MinimumLength = PostContentMinLength, ErrorMessage = StringLengthErrorMessage)]
+        public string Content { get; set; } = null!;
+
+
+        [Required(ErrorMessage = RequiredFieldMessage)]
+        public int CategoryId { get; set; }
+    }
+}
