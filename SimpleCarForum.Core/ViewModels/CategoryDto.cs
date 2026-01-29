556f5705cc147377b90e289b5e1ff6cadd4c6a50
AlexAndonov <114021498+AlexAndonov@users.noreply.github.com>
2026-01-29 23:30:04 +0200

    Add CategoryDto and CategoryCreateDto

---
+++ SimpleCarForum.Core/ViewModels/CategoryDto.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using static SimpleCarForum.Infra.Data.Constants.ValidationConstants;
+
+namespace SimpleCarForum.Core.ViewModels
+{
+	public class CategoryDto
+	{
+		public int Id { get; set; }
+
+		[Required]
+		[StringLength(CategoryNameMaxLength, MinimumLength = CategoryNameMinLength, ErrorMessage = StringLengthErrorMessage)]
+		[Comment("Name of the category")]
+		public string Name { get; set; } = null!;
+
+		[Required]
+		[StringLength(CategoryDescriptionMaxLength, MinimumLength = CategoryDescriptionMinLength, ErrorMessage = StringLengthErrorMessage)]
+		[Comment("Description of the category")]
+		public string Description { get; set; } = null!;
+	}
+}
