68ca17dc8ca39349d4fcb46191fb3508530fe6e2
AlexAndonov <114021498+AlexAndonov@users.noreply.github.com>
2026-02-14 15:46:17 +0200

    Add GetForEditAsync and GetPostsByCategoryAsync in IPostService

--- SimpleCarForum.Core/Contracts/IPostService.cs
+++ SimpleCarForum.Core/Contracts/IPostService.cs
@@ -1,4 +1,4 @@
-﻿using SimpleCarForum.Core.ViewModels;
+﻿using SimpleCarForum.Core.ViewModels.Post;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -8,9 +8,11 @@ namespace SimpleCarForum.Core.Contracts
     public interface IPostService
     {
         Task<IEnumerable<PostDto>> GetAllAsync();
+        Task<IEnumerable<PostDto>> GetPostsByCategoryAsync(int? categoryId);
         Task<PostDto?> GetByIdAsync(Guid id);
-        Task<PostDto> CreateAsync(PostCreateDto model, string userId);
-        Task<PostDto?> UpdateAsync(PostEditDto model);
+		Task<PostEditDto?> GetForEditAsync(Guid id);
+		Task<PostDto> CreateAsync(PostCreateDto model, string userId);
+        Task<bool> UpdateAsync(PostEditDto model);
         Task<bool> DeleteAsync(Guid id);
         Task<bool> IsOwnerAsync(Guid postId, string userId);
     }
