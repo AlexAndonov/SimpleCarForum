a4fafdc9f3b16464e35c2766228e13ab4170f73a
AlexAndonov <114021498+AlexAndonov@users.noreply.github.com>
2026-02-14 15:52:10 +0200

    Add IPostService and ICommentServiceto IoC

--- SimpleCarForum/ServiceExtensionsCollection/ServiceExtensions.cs
+++ SimpleCarForum/ServiceExtensionsCollection/ServiceExtensions.cs
@@ -7,29 +7,31 @@ using SimpleCarForum.Infra.Data.Models;
 
 namespace Microsoft.Extensions.DependencyInjection
 {
-    public static class ServiceExtensions
-    {
-        public static IServiceCollection AddApplicationServices(this IServiceCollection services)
-        {
-            services.AddScoped<ICategoryService, CategoryService>();
-            return services;
-        }
+	public static class ServiceExtensions
+	{
+		public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+		{
+			services.AddScoped<ICategoryService, CategoryService>();
+			services.AddScoped<IPostService, PostService>();
+			services.AddScoped<ICommentService, CommentService>();
+			return services;
+		}
 
-        public static IServiceCollection AddApplicationIdentity(this IServiceCollection services)
-        {
-            services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = false)
-                .AddRoles<IdentityRole>()
-                .AddEntityFrameworkStores<ApplicationDbContext>();
-            return services;
-        }
+		public static IServiceCollection AddApplicationIdentity(this IServiceCollection services)
+		{
+			services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = false)
+				.AddRoles<IdentityRole>()
+				.AddEntityFrameworkStores<ApplicationDbContext>();
+			return services;
+		}
 
-        public static IServiceCollection AddApplicationDbContext(this IServiceCollection services, IConfiguration configuration)
-        {
-            var connectionString = configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
-            services.AddDbContext<ApplicationDbContext>(options =>
-                options.UseSqlServer(connectionString));
+		public static IServiceCollection AddApplicationDbContext(this IServiceCollection services, IConfiguration configuration)
+		{
+			var connectionString = configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+			services.AddDbContext<ApplicationDbContext>(options =>
+				options.UseSqlServer(connectionString));
 
-            return services;
-        }
-    }
+			return services;
+		}
+	}
 }
