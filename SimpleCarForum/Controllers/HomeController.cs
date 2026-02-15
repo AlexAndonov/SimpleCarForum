61882929fa8ac2e6719bfda59f32933cce8b9e72
AlexAndonov <114021498+AlexAndonov@users.noreply.github.com>
2026-02-15 21:44:05 +0200

    Refactor Index methods in Home Controller

--- SimpleCarForum/Controllers/HomeController.cs
+++ SimpleCarForum/Controllers/HomeController.cs
@@ -1,25 +1,40 @@
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
+using SimpleCarForum.Core.Contracts;
 using SimpleCarForum.Models;
+using SimpleCarForum.ViewModels;
 
 namespace SimpleCarForum.Controllers
 {
-    public class HomeController : Controller
-    {
-        public IActionResult Index()
-        {
-            return View();
-        }
+	public class HomeController : Controller
+	{
+		private readonly ICategoryService categoryService;
+		private readonly IPostService postService;
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
+		public HomeController(ICategoryService _categorService, IPostService _postService)
+		{
+			categoryService = _categorService;
+			postService = _postService;
+		}
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
-    }
+		public async Task<IActionResult> Index()
+		{
+			var categories = await categoryService.GetAllAsync();
+			var posts = await postService.GetAllAsync();	
+			var model = new HomeViewModel()
+			{
+				Categories = categories,
+				Posts = posts
+			};
+
+			return View(model);
+		}
+
+
+		[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+		public IActionResult Error()
+		{
+			return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+		}
+	}
 }
