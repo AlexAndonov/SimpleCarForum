3be616e5ca22b609ad6a43762f34bfdeb4242a61
AlexAndonov <114021498+AlexAndonov@users.noreply.github.com>
2026-02-15 21:46:45 +0200

    Add comment controller methods with implementation

--- SimpleCarForum/Controllers/CommentController.cs
+++ SimpleCarForum/Controllers/CommentController.cs
@@ -1,8 +1,11 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.DotNet.Scaffolding.Shared.Project;
+using Microsoft.EntityFrameworkCore;
 using SimpleCarForum.Core.Contracts;
+using SimpleCarForum.Core.Services;
 using SimpleCarForum.Core.ViewModels.Comment;
+using System.Runtime.InteropServices;
 using System.Security.Claims;
 
 namespace SimpleCarForum.Controllers
@@ -22,10 +25,10 @@ namespace SimpleCarForum.Controllers
 		{
 			if (ModelState.IsValid == false)
 			{
-				return RedirectToAction("Details", "Post", new {id = model.PostId});
+				return RedirectToAction("Details", "Post", new { id = model.PostId });
 			}
 
-			var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+			var userId = GetUserId();
 
 			if (userId == null)
 			{
@@ -34,35 +37,137 @@ namespace SimpleCarForum.Controllers
 
 			await service.CreateAsync(model, userId);
 
-			return RedirectToAction("Details", "Post", new {id = model.PostId});
+			return RedirectToAction("Details", "Post", new { id = model.PostId });
 		}
 
-		[HttpPost]
+		[HttpGet]
 		[Authorize]
 		public async Task<IActionResult> Delete(Guid id)
 		{
-			var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+			var comment = await service.GetByIdAsync(id);
+			if (comment == null)
+			{
+				return NotFound();
+			}
 
+			var userId = GetUserId();
 			if (userId == null)
 			{
 				return Unauthorized();
 			}
 
-			bool isOwner = await service.IsOwnerAsync(id, userId);
-
-			if (isOwner == false)
+			if (!IsOwnerOrAdmin(userId, comment.AuthorId))
 			{
 				return Forbid();
 			}
 
-			bool success = await service.DeleteAsync(id);
+			var model = new CommentEditDto
+			{
+				Id = comment.Id,
+				Content = comment.Content,
+				PostId = comment.PostId
+			};
 
-			if (success == false)
+			return View(model);
+		}
+
+		[HttpPost]
+		[Authorize]
+		public async Task<IActionResult> Delete(Guid id, Guid postId)
+		{
+			var userId = GetUserId();
+			if (userId == null)
+			{
+				return Unauthorized();
+			}
+
+			var comment = await service.GetByIdAsync(id);
+			if (comment == null)
 			{
 				return NotFound();
 			}
 
-			return RedirectToAction("Details", "Post", new {id});
+			if (!IsOwnerOrAdmin(userId, comment.AuthorId))
+			{
+				return Forbid();
+			}
+
+			await service.DeleteAsync(id);
+
+			return RedirectToAction("Details", "Post", new { id = postId });
 		}
+
+		[HttpGet]
+		[Authorize]
+		public async Task<IActionResult> Edit(Guid id)
+		{
+			var comment = await service.GetByIdAsync(id);
+			if (comment == null)
+			{
+				return NotFound();
+			}
+
+			var userId = GetUserId();
+			if (userId == null)
+			{
+				return Unauthorized();
+			}
+
+			if (comment.AuthorId != userId)
+			{
+				return Forbid();
+			}
+
+			var model = new CommentEditDto()
+			{
+				Id = comment.Id,
+				Content = comment.Content,
+				PostId = comment.PostId,
+			};
+
+			return View(model);
+		}
+
+		[HttpPost]
+		[Authorize]
+		public async Task<IActionResult> Edit(CommentEditDto model)
+		{
+			if (!ModelState.IsValid)
+			{
+				return View(model);
+			}
+
+			var userId = GetUserId();
+			if (userId == null)
+			{
+				return Unauthorized();
+			}
+
+			var comment = await service.GetByIdAsync(model.Id);
+			if (comment == null)
+			{
+				return NotFound();
+			}
+
+			if (userId != comment.AuthorId)
+			{
+				return Forbid();
+			}
+
+			await service.UpdateAsync(model);
+
+			return RedirectToAction("Details", "Post", new { id = model.PostId });
+		}
+
+		private string? GetUserId()
+		{
+			return User.FindFirstValue(ClaimTypes.NameIdentifier);
+		}
+
+		private bool IsOwnerOrAdmin(string? userId, string authorId)
+		{
+			return userId == authorId || User.IsInRole("Admin");
+		}
+
 	}
 }
